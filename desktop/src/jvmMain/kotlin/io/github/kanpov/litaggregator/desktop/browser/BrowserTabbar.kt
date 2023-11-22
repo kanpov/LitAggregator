e55fe6ba4a0887d4be5733937592f5702fd7d543
kanpov <karpovanton729@gmail.com>
2023-11-22 20:15:09 +0300

    Initiate thermonuclear bombing of codebase

--- desktop/src/jvmMain/kotlin/io/github/kanpov/litaggregator/desktop/browser/BrowserTabbar.kt
+++
@@ -1,9 +0,0 @@
-package io.github.kanpov.litaggregator.desktop.browser
-
-import androidx.compose.runtime.Composable
-import io.github.kanpov.litaggregator.engine.profile.ProfileManager
-
-@Composable
-fun BrowserTabbar(manager: ProfileManager) {
-
-}
